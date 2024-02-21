47737368d21dc8805dc80d3f30082aeb770cf974
Yiyi Xu <xuyiyi0516@gmail.com>
2024-02-21 04:17:58 -0500

    finished project 1

--- C/projects/project01/autocomplete.c
+++ C/projects/project01/autocomplete.c
@@ -13,14 +13,20 @@ int compare_terms(const void *a, const void *b) {
     return strcmp(term_a->term, term_b->term);
 }
 
-void read_in_terms(term **terms, int *pnterms, char *filename) {
-    
-    FILE *file = fopen(filename, "r");
-
-    if(file == NULL) {
-        printf("Error opening file %s\n", filename);
-        exit(1);
+int compare_weights(const void *a, const void *b) {
+    const term *term_a = (const term *)a;
+    const term *term_b = (const term *)b;
+    if(term_a->weight < term_b->weight) {
+        return 1;
+    } else if(term_a->weight > term_b->weight) {
+        return -1;
+    } else {
+        return 0;
     }
+}
+
+void read_in_terms(term **terms, int *pnterms, char *filename) {
+    FILE *file = fopen(filename, "r");
 
     fscanf(file, "%d", pnterms);
 
@@ -35,13 +41,45 @@ void read_in_terms(term **terms, int *pnterms, char *filename) {
     qsort(*terms, *pnterms, sizeof(term), compare_terms);
 }
 
-int main() {
-    term *terms;
-    int nterms;
-    read_in_terms(&terms, &nterms, "cities.txt");
-    printf("%d\n", nterms);
-    for(int i = 0; i < 5; i++) {
-        printf("%s %lf\n", terms[i].term, terms[i].weight);
+int lowest_match(term *terms, int nterms, char *substr) {
+    int left = 0;
+    int right = nterms - 1;
+    while(left<=right) {
+        int mid = (left + right) /2;
+        if(strncmp(terms[mid].term, substr, strlen(substr)) >= 0) {
+            right = mid - 1;
+        } else {
+            left = mid + 1;
+        }
     }
-    return 0;
+    return left;
+}
+
+int highest_match(term *terms, int nterms, char *substr) {
+    int left = 0;
+    int right = nterms - 1;
+    while(left<=right) {
+        int mid = (left + right) /2;
+        if(strncmp(terms[mid].term, substr, strlen(substr)) > 0) {
+            right = mid - 1;
+        } else {
+            left = mid + 1;
+        }
+    }
+    return right;
+}
+
+void autocomplete(term **answer, int *n_answer, term *terms, int nterms, char *substr) {
+    int left = lowest_match(terms, nterms, substr);
+    int right = highest_match(terms, nterms, substr);
+
+    *n_answer = right - left + 1;
+
+    *answer = (term *) malloc(*n_answer * sizeof(term));
+
+    for(int i = 0; i < *n_answer; i++) {
+        (*answer)[i] = terms[left + i];
+    }
+
+    qsort(*answer, *n_answer, sizeof(term), compare_weights);
 }
